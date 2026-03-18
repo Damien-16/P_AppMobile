5344190c2b72b7ef2a6abb83c79616eb1b5acccd
Damien Chabal <pv77ngi@eduvaud.ch>
2026-03-18 16:23:56 +0100

    new initial commit

---
+++ P_AppMobile-ReadMe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace P_AppMobile_ReadMe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ P_AppMobile-ReadMe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace P_AppMobile_ReadMe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
