ea7353b464e49f3e452cb372272ea0112ece8a75
Damien Chabal <pv77ngi@eduvaud.ch>
2026-03-25 13:52:05 +0100

    add FlashCards Project

---
+++ FlashCards/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashCards
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FlashCards/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashCards
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
