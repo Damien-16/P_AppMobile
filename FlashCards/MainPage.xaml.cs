945a0ab4b32f74157b7e51885231411b79175694
Damien Chabal <pv77ngi@eduvaud.ch>
2026-05-06 13:32:21 +0200

    chore (flashcards) : affichage de la page d'accueil

--- FlashCards/MainPage.xaml.cs
+++ FlashCards/MainPage.xaml.cs
@@ -12,5 +12,11 @@
             // Navigation vers la page des decks
             await Shell.Current.GoToAsync("//DecksPage");
         }
+
+        private async void OnLearnClicked(object sender, EventArgs e)
+        {
+            // Navigation vers DecksPage aussi pour choisir quoi apprendre
+            await Shell.Current.GoToAsync("//DecksPage");
+        }
     }
 }
